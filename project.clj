2e60390e53578ebb76461d8f2dc7ad9a41d2f800
Hayden Sartoris <hsartoris@bard.edu>
2020-07-10 14:05:22 +0000

    working configuration and self-signed ssl

--- project.clj
+++ project.clj
@@ -3,7 +3,18 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/data.json "0.2.6"]
+                 [clj-http "3.10.1"]
+                 [metosin/compojure-api "2.0.0-alpha31"]
+                 [aero "1.1.6"]
+                 [ring/ring-jetty-adapter "1.7.0"]]
+  :plugins [[lein-ring "0.12.5"]]
+  :ring {:handler brightspace-adapter.core/app
+         :adapter {:ssl? true
+                   :ssl-port 8443
+                   :keystore "selfsigned.jks"
+                   :key-password "changeit"}}
   :main ^:skip-aot brightspace-adapter.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/brightspace_adapter/core.clj
+++ src/brightspace_adapter/core.clj
@@ -1,7 +1,35 @@
 (ns brightspace-adapter.core
+  (:require [compojure.api.sweet :refer :all]
+            [aero.core :as aero]
+            [clojure.pprint :refer [pprint]]
+            [ring.util.http-response :refer [ok]])
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def debug true)
+
+; read configuration from resources/config.edn
+; by using clojure.java.io/resource we also can retrieve from classpath
+(def config (-> (clojure.java.io/resource "config.edn")
+                (aero/read-config {:profile :dev})))
+
+(when debug (pprint config))
+
+(def app
+  (api
+    (context 
+      (:base-url config) []
+      (GET "/" []
+           (ok "Yup")))))
+
+
+;(def app
+;  (api
+;    (context 
+;      "/brightspace" []
+
+
+
+;(defn -main
+;  "I don't do a whole lot ... yet."
+;  [& args]
+;  (println "Hello, World!"))
